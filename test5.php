5f5c325100ac8453bb292d2345425935ef6f7bf8
이혜령 <84839619+leehyeryeong@users.noreply.github.com>
2022-04-21 10:22:54 +0900

    Create test5.php

---
+++ test5.php
@@ -0,0 +1,17 @@
+<?php
+//score=87일 때 A,B,C,D,F 출력
+$score = 87;
+
+if($score>=90) {
+    echo "A";
+} else if($score>=80) {
+    echo "B";
+} else if($score>=70) {
+    echo "C";
+} else if($score>=60) {
+    echo "D";
+} else {
+    echo "F";
+} //end of if
+
+?>
