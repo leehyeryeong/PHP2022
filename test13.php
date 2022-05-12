48e18b3dd9123d26b2adc717076785e31da4619b
이혜령 <84839619+leehyeryeong@users.noreply.github.com>
2022-05-12 09:01:05 +0900

    Add files via upload

---
+++ test13.php
@@ -0,0 +1,20 @@
+<?php
+$arr = array();
+$arr[0] = "apple";
+$arr[1] = "banana";
+$arr[2] = "melon";
+
+for($i=0; $i<3; $i++) {
+    echo $arr[$i]." ";
+} //end of for
+
+?>
+<br />
+<?php
+$arr2 = array("strawberry", "mango", "orange");
+
+for($i=0; $i<3; $i++) {
+    echo $arr2[$i]." ";
+} //end of for
+
+?>
