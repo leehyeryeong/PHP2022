e79cc959301e9670c1fe3af20c7461bee3f47878
이혜령 <84839619+leehyeryeong@users.noreply.github.com>
2022-05-26 09:42:25 +0900

    Create test17.php

---
+++ test17.php
@@ -0,0 +1,13 @@
+<?php
+//$dir = opendir('.'); //현재 디렉토리를 열어서 dir에 대입한다.
+$dir = opendir('C:\Bitnami\wampstack-8.1.4-0\apache2');
+    while($file=readdir($dir)) {
+        echo $file."<br />";
+    } //end of while
+closedir($dir);
+?>
+<br />
+<?php
+$list = scandir('./'); //현재 디렉토리에 있는 파일과 폴더를 list(배열)에 대입
+var_dump($list);
+?>
