e1c781c2945393253b2569a4f13fd72e9f005603
이혜령 <84839619+leehyeryeong@users.noreply.github.com>
2022-06-16 10:19:50 +0900

    Update menu.php

--- phpPrj/menu.php
+++ phpPrj/menu.php
@@ -1,4 +1,6 @@
-<a href="multi_form.html">구구단</a>
-<a href="score_form.html">성적 구하기</a>
-<a href="sum">입력받은 두 수 사이의 합 구하기</a>
-<a href="login">로그인 구현하기</a>
+<a href="multi_form.html">구구단 |</a>
+<a href="score_form.html">성적 구하기 |</a>
+<a href="sum_form.html">입력받은 두 수 사이의 합 구하기 |</a>
+<a href="snack_count_form.html">과자 가격 구하기 |</a>
+<a href="day_input_form.html">요일 구하기 |</a>
+<a href="login.html">로그인 구현하기 |</a>
